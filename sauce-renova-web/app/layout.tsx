ca5f81f0e321bc82daae5c10150af06a827e8a46
Selam Bel <116879445+SelamBel@users.noreply.github.com>
2026-06-24 22:29:30 +0200

    nav bar fixes

--- sauce-renova-web/app/layout.tsx
+++ sauce-renova-web/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>
+      <body className="bg-cream">
         <Navbar />
         {children}
       </body>
